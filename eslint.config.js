bfd0a3bb7348a34a497cd08f29a794d18ccba638
Aayushman Singh <kremzylo@gmail.com>
2026-05-21 13:45:10 +0530

    chore: wire eslint + prettier

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/', '.wrangler/', 'node_modules/', 'worker-configuration.d.ts'],
+  },
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
+  },
+  prettier,
+);
